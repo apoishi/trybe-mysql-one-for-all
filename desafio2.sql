e1a9c21a77cfa6980df62d41113b223894261f92
Ariane Padilha <ariane.padilha@outlook.com.br>
2022-09-24 00:33:44 -0300

    feat: requirement 02 done

---
+++ desafio2.sql
@@ -0,0 +1,9 @@
+
+SELECT
+  COUNT(DISTINCT s.song_id) AS cancoes,
+  COUNT(DISTINCT ar.artist_id) AS artistas,
+  COUNT(DISTINCT al.album_id) AS albuns
+FROM
+  SpotifyClone.songs AS s,
+  SpotifyClone.artists AS ar,
+  SpotifyClone.albums AS al;
